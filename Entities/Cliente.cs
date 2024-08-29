6c89be8bbb7a4fc840fbd33763e9397828873233
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-08-29 10:15:36 -0300

    Implementadas as classes Funcionario e Cliente que herdam de Pessoa

---
+++ Entities/Cliente.cs
@@ -0,0 +1,16 @@
+namespace CadastroLivrosPOO;
+
+public class Cliente : Pessoa
+{
+    public string Endereco { get; set; }
+
+    public Cliente(string nome, string cpf, string endereco) : base(nome, cpf)
+    {
+        Endereco = endereco;
+    }
+
+    public override void ObterIdentificacao()
+    {
+        Console.WriteLine($"Cliente: {Nome}, CPF: {CPF}, Endereço: {Endereco}");
+    }
+}
---
+++ Entities/Funcionario.cs
@@ -0,0 +1,16 @@
+namespace CadastroLivrosPOO;
+
+public class Funcionario : Pessoa
+{
+    public string Cargo { get; set; }
+
+    public Funcionario(string nome, string cpf, string cargo) : base(nome, cpf)
+    {
+        Cargo = cargo;
+    }
+
+    public override void ObterIdentificacao()
+    {
+        Console.WriteLine($"Funcionario: {Nome}, CPF: {CPF}, Cargo: {Cargo}");
+    }
+}
